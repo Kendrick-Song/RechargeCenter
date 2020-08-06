6802e26de324ffebff25096a425fe9a69e29c848
kendrick_song <kendrick818@outlook.com>
2020-08-06 11:35:09 +0800

    init

---
+++ app/src/main/java/com/example/rechargecenter/RechargeCenterActivity.java
@@ -0,0 +1,36 @@
+package com.example.rechargecenter;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class RechargeCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recharge_center);
+
+        //隐藏系统自带标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //设置标题
+        TextView title_tv = findViewById(R.id.title_tv);
+        title_tv.setText("充值中心");
+
+        //返回Button
+        ImageButton title_btn_back = findViewById(R.id.title_btn_back);
+        title_btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
