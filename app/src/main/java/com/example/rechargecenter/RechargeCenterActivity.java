da1919c8361d931dff065abc17bbbb34ff037f70
kendrick_song <kendrick818@outlook.com>
2020-08-06 14:15:12 +0800

    add tab+pager

--- app/src/main/java/com/example/rechargecenter/RechargeCenterActivity.java
+++ app/src/main/java/com/example/rechargecenter/RechargeCenterActivity.java
@@ -1,13 +1,26 @@
 package com.example.rechargecenter;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RechargeCenterActivity extends AppCompatActivity {
+
+    List<Fragment> fragments = new ArrayList<>();
+    List<String> titles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +43,37 @@ public class RechargeCenterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        TabLayout rechargeTab = findViewById(R.id.recharge_tab);
+        ViewPager rechargePager = findViewById(R.id.recharge_pager);
+        titles.add("充话费");
+        titles.add("充流量");
+        titles.add("充视频会员");
+        fragments.add(new RechargeHfFragment());
+        fragments.add(new RechargeLlFragment());
+        fragments.add(new RechargeSpFragment());
+        rechargePager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                super.destroyItem(container, position, object);
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles.get(position);
+            }
+        });
+        rechargeTab.setupWithViewPager(rechargePager);
     }
 }
