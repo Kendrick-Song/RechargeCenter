789b5eec3a73ac730a1aba3f9750a5a8f04668f7
Erin_522 <1219393063@qq.com>
2020-08-10 10:15:57 +0800

    HF.xml完善

--- app/src/main/java/com/example/rechargecenter/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/rechargecenter/ui/dashboard/DashboardFragment.java
@@ -15,20 +15,12 @@ import com.example.rechargecenter.R;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_order, container, false);
-        final TextView textView = root.findViewById(R.id.text_order);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
